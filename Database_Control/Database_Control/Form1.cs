61eb5d26845fd4f073aaa93ad0c8ff3e2f2d354e
Ian <irobot9803@gmail.com>
2023-09-07 11:02:11 -0500

    sql and list create

--- Database_Control/Database_Control/Form1.cs
+++ Database_Control/Database_Control/Form1.cs
@@ -1,3 +1,4 @@
+using System.Data.SqlClient;
 using System.Runtime.CompilerServices;
 
 namespace Database_Control
@@ -32,6 +33,35 @@ namespace Database_Control
             DisplayControl = new WindowData(this, new StatusType(StatusType.DefaultAdmin));
             SetWindow(WindowType.Delivery, new Dictionary<string, string>());
         }
+
+        public enum List { OrderList, ListDisplay, OrderDiplay_Company, OrderDisplay_Product }
+        public FlowLayoutPanel GetList(List Item)
+        {
+            switch (Item)
+            {
+                case List.OrderList:
+                    return OrderList;
+                case List.ListDisplay:
+                    return ItemList;
+                case List.OrderDiplay_Company:
+                    return CompanyList;
+                case List.OrderDisplay_Product:
+                    return ProductList;
+                default:
+                    return OrderList;
+            }
+        }
+    }
+
+    public class SQL
+    {
+        private SqlConnection com;
+
+        public SQL()
+        {
+            com = new SqlConnection(@"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb;User ID=sa;Password=demol23");
+            com.Open();
+        }
     }
 
     public class StatusType
@@ -60,6 +90,17 @@ namespace Database_Control
             int clearBit = ~(1 << pos);
             int mask = Current & clearBit;
             return mask | (NewBit << pos);
+        }
+
+        public static string RandomString(int Length)
+        {
+            string Out = "";
+            Random Rng = new Random();
+            for (int i = 1; i < Length; i++)
+            {
+                Out += (char)Rng.Next((int)'!', (int)'~');
+            }
+            return Out;
         }
     }
 
@@ -92,6 +133,40 @@ namespace Database_Control
             return false;
         }
 
+        private static void DeleteAllConents(FlowLayoutPanel list)
+        {
+            list.Controls.Clear();
+        }
+
+        private static void AddNewConentItem(FlowLayoutPanel list, string GroupName, int Height = 70)
+        {
+            Panel Item = new Panel();
+            GroupBox Box = new GroupBox();
+            int Width = (list.Size.Width - 20);
+            Item.BackColor = Color.White;
+            Item.BorderStyle = BorderStyle.Fixed3D;
+            int Down = 3;
+            for (int i = 0; i < list.Controls.Count; i++)
+            {
+                Down += list.Controls[i].Size.Height;
+            }
+            Item.Location = new Point(3, Down);
+            Item.Name = "panel#" + StatusType.RandomString(5);
+            Item.Size = new Size(Width, Height);
+            Item.TabIndex = 0;
+            Item.Controls.Add(Box);
+
+            Box.Dock = DockStyle.Fill;
+            Box.Location = new Point(0, 0);
+            Box.Name = "groupBox";
+            Box.Size = new Size((int)(Width * 0.9f), (int)(Height * 0.8f));
+            Box.TabIndex = 0;
+            Box.TabStop = false;
+            Box.Text = GroupName;
+
+            list.Controls.Add(Item);
+        }
+
         private static bool OpenLogin(MainForm Form, StatusType Status, Dictionary<string, string> DataIn)
         {
             return true;
@@ -99,6 +174,11 @@ namespace Database_Control
 
         private static bool OpenDelivery(MainForm Form, StatusType Status, Dictionary<string, string> DataIn)
         {
+            DeleteAllConents(Form.GetList(MainForm.List.OrderList));
+            for (int i = 0; i < 10; i++)
+            {
+                AddNewConentItem(Form.GetList(MainForm.List.OrderList), "Test: " + i);
+            }
             return true;
         }
 
