ca02938d608f0faa9ecd2266a921dadae8a8de83
Guti2020xx <54343329+Guti2020xx@users.noreply.github.com>
2023-10-12 11:40:50 -0500

    Image to String Decode/Encode
    Simple code to encode and image to a base 64 string and decoding a base 64 string to a byte array.

---
+++ Database_Control/Database_Control/EncodeDecodeImageString.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+class ImageStrinEncoderDecoder
+{
+    public static string encodeImageToString(string filePath)
+    {
+        byte[] imageArr;
+        string encodedString;
+        try
+        {
+            imageArr = File.ReadAllBytes(filePath);
+            encodedString = Convert.ToBase64String(imageArr);
+            return encodedString;
+        }
+        catch (Exception ex)
+        {
+            // modify this to see if you can integrate into UI
+            Console.WriteLine("Error: " + ex.Message);
+            return null;
+        }
+    }
+
+    public static byte[] decodeBase64StringToImage( string base64String)
+    {
+        byte[] img = Convert.FromBase64String(base64String);
+        return img;
+    }
+
+}
